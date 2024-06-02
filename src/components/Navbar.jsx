bbdd133391b43a4228dfd67a6630ede416179322
mahtab <161712272+hrmahtab01@users.noreply.github.com>
2024-06-02 13:43:29 +0600

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Navbar = () => {
+  
+  
+
   return (
     <>
-      <nav>
-        <div className='w-container mx-auto'>
-      <div className='flex justify-between items-center'>
-      <img src="images/Logo.png" alt="logo" />
-      <div className='flex'>
-      <ul className='flex gap-x-6 '>
-            <li className='hover:text-hvc-500'>Home</li>
-            <li className='hover:text-red-500'>Our services</li>
-            <li className='hover:text-red-500'>About Us</li>
-            <li className='hover:text-red-500'>What’s new?</li>
-            </ul>
-            <button>Contact us</button>
-
+      <div className={`${darkmode && 'dark'}`}>
+        <nav className={darkmode ? 'dark:bg-black' : ''}>
+          <div className='w-container mx-auto'>
+            <div className='flex justify-between items-center dark:text-[#FFCC00]'>
+              <img src="images/Logo.png" alt="logo" />
+              <div className='flex items-center gap-24'>
+                <ul className='flex gap-x-6'>
+                  <li className='hover:text-red-500'>Home</li>
+                  <li className='hover:text-red-500'>Our services</li>
+                  <li className='hover:text-red-500'>About Us</li>
+                  <li className='hover:text-red-500'>What’s new?</li>
+                </ul>
+                <button className='ml-10px'>Contact us</button>
+                <button className='w-16 h-16 bottom-16 right-16 bg-[#FFCC00] dark:bg-teal-500 rounded-full dark:text-black font-semibold ml-10px' onClick={toggleDarkmode}>
+                  {darkmode ? "LGT" : "DRK"}
+                </button>
+              </div>
+            </div>
+          </div>
+        </nav>
       </div>
-      </div>
-        </div>
-      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
-
+export default Navbar;
